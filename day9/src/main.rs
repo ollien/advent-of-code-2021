95a984fac4b4f19f8059f3d32d7e1f75a8cdd184
Nick Krichevsky <nick@ollien.com>
2021-12-09 01:10:56 -0500

    Refactor day 9 part 1 in preperation for part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,55 +2,74 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("Tried to get out of bounds row, {0}")]
+    RowOutOfRange(usize),
+}
+
+fn find_low_point_indices(
+    input: &[Vec<u32>],
+    depth: usize,
+) -> Result<impl Iterator<Item = usize> + '_, Error> {
+    let maybe_row = input.get(depth);
+    if maybe_row.is_none() {
+        return Err(Error::RowOutOfRange(depth));
+    }
+
+    let row = maybe_row.unwrap();
+    let iter = row.iter().enumerate().filter_map(move |(i, &item)| {
+        let adjacent = {
+            let mut res = vec![
+                row.get(i + 1),
+                input.get(depth + 1).map(|below_row| {
+                    below_row
+                        .get(i)
+                        .expect("below row does not match row length")
+                }),
+            ];
+
+            if i > 0 {
+                let left = row.get(i - 1);
+                res.push(left);
+            }
+            if depth > 0 {
+                let above = input.get(depth - 1).map(|above_row| {
+                    above_row
+                        .get(i)
+                        .expect("above row does not match row length")
+                });
+                res.push(above);
+            }
+
+            res
+        };
+
+        if adjacent
+            .into_iter()
+            .filter(Option::is_some)
+            .all(|adjacent_item| item < *adjacent_item.unwrap())
+        {
+            Some(i)
+        } else {
+            None
+        }
+    });
+
+    Ok(iter)
+}
 
 fn part1(input: &[Vec<u32>]) -> u32 {
     input
         .iter()
         .enumerate()
         .map(|(depth, row)| {
-            println!("Depth: {}", depth);
-            row.iter()
-                .enumerate()
-                .map(|(i, &item)| {
-                    let adjacent = {
-                        let mut res = vec![
-                            row.get(i + 1),
-                            input.get(depth + 1).map(|below_row| {
-                                below_row
-                                    .get(i)
-                                    .expect("below row does not match row length")
-                            }),
-                        ];
-
-                        if i > 0 {
-                            let left = row.get(i - 1);
-                            res.push(left);
-                        }
-                        if depth > 0 {
-                            let above = input.get(depth - 1).map(|above_row| {
-                                above_row
-                                    .get(i)
-                                    .expect("above row does not match row length")
-                            });
-                            res.push(above);
-                        }
-
-                        res
-                    };
-
-                    // println!("{}, {}", depth, i);
-                    if adjacent
-                        .into_iter()
-                        .filter(Option::is_some)
-                        .all(|adjacent_item| item < *adjacent_item.unwrap())
-                    {
-                        // println!("{} => 1 + {}", item, height);
-                        1 + item
-                    } else {
-                        0
-                    }
-                })
-                .sum()
+            find_low_point_indices(input, depth)
+                .expect("failed to get row for depth analysis")
+                .map(|idx| row[idx] + 1)
+                .sum::<u32>()
         })
         .sum()
 }
