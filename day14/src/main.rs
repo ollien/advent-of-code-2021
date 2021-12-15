360990b0b1f55f168742da3957960c86027e510a
Nick Krichevsky <nick@ollien.com>
2021-12-14 21:42:31 -0500

    Clean up day 14 part 2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,11 +1,16 @@
+#![warn(clippy::all, clippy::pedantic)]
 use std::collections::HashMap;
-use std::{env, fs};
+use std::env;
+use std::fs;
 
-use nom::bytes::complete::{tag, take_while1};
-use nom::combinator::eof;
-use nom::multi::{many0, separated_list1};
-use nom::sequence::{pair, separated_pair, terminated};
-use nom::{character::complete::char, IResult};
+use nom::{
+    bytes::complete::{tag, take_while1},
+    combinator::eof,
+    multi::{many0, separated_list1};,
+    sequence::{pair, separated_pair, terminated},
+    character::complete::char,
+    IResult
+};
 
 fn parse_polymer(chunk: &str) -> IResult<&str, &str> {
     take_while1(|c: char| c.is_ascii_uppercase())(chunk)
@@ -26,6 +31,10 @@ fn parse_input(input: &str) -> IResult<&str, (&str, Vec<(&str, &str)>)> {
     )(input)
 }
 
+/// Get all of the pairs of chars in the template string, overlapping.
+/// This is equivalent (though it does allocate, but this is only used
+/// on a small string) to using slice::window(2), but this is not
+/// available for strings :(
 fn get_all_pairs(template: &str) -> Vec<&str> {
     let mut pairs = Vec::<&str>::new();
     for i in 0..template.len() - 1 {
@@ -37,6 +46,7 @@ fn get_all_pairs(template: &str) -> Vec<&str> {
 }
 
 fn run(template: &str, mappings: &HashMap<&str, char>, num_iterations: usize) -> u64 {
+    // Populate the counts of pairs with 0 for any pairs in a rule, and 1 for every pair in our template
     let mut pair_counts = mappings
         .keys()
         .copied()
@@ -44,49 +54,47 @@ fn run(template: &str, mappings: &HashMap<&str, char>, num_iterations: usize) ->
         .chain(get_all_pairs(template).into_iter().map(|pair| (pair, 1)))
         .collect::<HashMap<&str, u64>>();
 
-    let mut element_counts = HashMap::<char, u64>::new();
-    for c in template.chars() {
-        *element_counts.entry(c).or_insert(0) += 1
-    }
+    let mut element_counts = template
+        .chars()
+        .map(|c| (c, 0))
+        .collect::<HashMap<char, u64>>();
 
     for _ in 0..num_iterations {
-        let pair_iter = pair_counts.iter().filter(|(_, &count)| count > 0);
-        let mut rule_application_counts = HashMap::<String, i64>::new();
-        for (pair, &count) in pair_iter {
+        let non_zero_count_pairs = pair_counts.iter().filter(|(_, &count)| count > 0);
+        let mut next_pair_counts = pair_counts.clone();
+        for (pair, &count) in non_zero_count_pairs {
             let new_char = mappings
                 .get(pair)
                 .unwrap_or_else(|| panic!("could not find mapping for rule {}", pair));
 
             *element_counts.entry(*new_char).or_insert(0) += count;
-            *rule_application_counts.entry(pair.to_string()).or_insert(0) -=
-                i64::try_from(count).unwrap();
+            // One of each of these pairs will no longer exist
+            *next_pair_counts.entry(pair).or_insert(0) -= count;
 
+            // ...but there will be newly formed pairs to add in, two per pair we removed
             for (i, c) in pair.chars().enumerate() {
+                // i will always be 0 or 1 here (since it's a pair of numbers, which is true by the parsing logic)
+                // passed into here
                 let rule_output = if i == 0 {
                     format!("{}{}", c, new_char)
                 } else {
                     format!("{}{}", new_char, c)
                 };
 
-                *rule_application_counts.entry(rule_output).or_insert(0) +=
-                    i64::try_from(count).unwrap();
+                let current_pair_count = next_pair_counts
+                    .get_mut(rule_output.as_str())
+                    .unwrap_or_else(|| {
+                        panic!(
+                            "somehow produced pair {} which was being tracked (and thus not in the rules map)",
+                            pair
+                        )
+                    });
+
+                *current_pair_count += count;
             }
         }
 
-        for (pair, count) in rule_application_counts {
-            let current_pair_count = pair_counts.get_mut(pair.as_str()).unwrap_or_else(|| {
-                panic!(
-                    "somehow produced pair {} which is not in the rules map",
-                    pair
-                )
-            });
-
-            if count > 0 {
-                *current_pair_count = current_pair_count.saturating_add(count as u64)
-            } else {
-                *current_pair_count = current_pair_count.saturating_sub(-count as u64)
-            }
-        }
+        pair_counts = next_pair_counts;
     }
 
     element_counts.values().max().unwrap() - element_counts.values().min().unwrap()
@@ -99,10 +107,8 @@ fn main() {
     let mappings = raw_mappings
         .into_iter()
         .map(|(rule, mapping)| {
-            if mapping.len() != 1 {
-                // This could be done in the parser but nom was giving me difficulty and this was easier
-                panic!("Rules should only have a length of one");
-            }
+            assert_eq!(rule.len(), 2, "Rule inputs should have length of 2");
+            assert_eq!(mapping.len(), 1, "Rule outputs should have length of 1");
 
             (rule, mapping.chars().next().unwrap())
         })
